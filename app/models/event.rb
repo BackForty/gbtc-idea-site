8509d3195d68cd23e38313a8f9fdb8e7d9e6f23e
Ed Schmalzle <ed@nerded.net>
2013-05-24 13:08:29 -0400

    add validations for required event attributes

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,8 @@ class Event < ActiveRecord::Base
   attr_accessible :title, :description, :date
   has_many :ideas, dependent: :destroy
 
+  validates :title, :description, :date, presence: true
+
   def self.future
     where(['date >= ?', Date.today])
   end
